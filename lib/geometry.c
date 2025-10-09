dbf69b54117ae0881808446741840b545103e298
Andrea Guidotti <guidot2001@gmail.com>
2025-10-10 01:01:40 +0200

    Improve readability and documentation in geometry

--- lib/geometry.c
+++ lib/geometry.c
@@ -4,95 +4,144 @@
 
 #include"../include/geometry.h"
 
-/* from cartesian to lexicographic coordinates
- e.g (1,2,3) with lattice Size = L is mapped in 1+2*L+3*L^2*/
+/* Convert cartesian coordinates to lexicographic index.
 
-void cart_to_lex(long int *lex, int *cartcoord, int dim, int L)
+   Given a lattice of size 'latticeSize' in each of 'dim' dimensions,
+   this function converts an array of cartesian coordinates
+   cartCoords[0..dim-1] into its corresponding lexicographic index.
+
+   Example:
+
+     For dim = 3 and latticeSize = L:
+       cartCoords = (x0, x1, x2)
+       => lex = x0 + x1*L + x2*L^2
+
+   This mapping allows a multidimensional lattice to be represented
+   as a one-dimensional array using lexicographic ordering.
+*/
+
+void cart_to_lex(long int *lex, int *cartcoord, int dim, int latticeSize)
 {
-    long int aux, res;
+    long int multiplier, lexIndex;
 
-    aux = 1;
-    res = 0;
+    multiplier = 1;
+    lexIndex = 0;
 
     for (int i = 0; i < dim; i++)
     {
-        res += cartcoord[i] * aux;
-        aux *= L;
+        lexIndex += cartcoord[i] * multiplier;
+        multiplier *= latticeSize;
     }
 
-    *lex = res;
+    *lex = lexIndex;
 }
 
-/* from lexicographic to cartesian coordinates
-e.g n with  lattice Size = L is mapped in: n/L^2 = cartcoord[2],...*/
+/* Convert a lexicographic index to cartesian coordinates.
 
-void lex_to_cart(long int lex,  int *cartcoord, int dim, int L)
+   Given a lattice of size 'latticeSize' in each of 'dim' dimensions,
+   this function converts an integer index 'lex' (0 <= lex < latticeSize^dim)
+   into its corresponding cartesian coordinates.
+
+   Example:
+
+     For dim = 3, latticeSize = L and lex = x0 + x1*L + x2*L^2: 
+     cartCoords[2] = lex/L^2, cartCoords[1] = (lex - x2*L^2)/L^1, 
+     cartCoords[1] = (lex - x2*L^2 -x1*L)/L^0, 
+     => cartCoords = (x0, x1, x2)
+*/
+
+void lex_to_cart(long int lex,  int *cartcoord, int dim, int latticeSize)
 {
-    long int aux;
-    aux = 1;
+    long int multiplier;
+    multiplier = 1;
 
     for (int i = 0; i < dim-1; i++)
     {
-        aux *= L;
+        multiplier *= latticeSize;
     }
 
-    for (int j = dim - 1; j >= 0; j--)
+    for (int d = dim - 1; d >= 0; d--)
     {
-        cartcoord[j] = (int)(lex / aux);
-        lex -= cartcoord[j] * aux;
-        aux /= L;
+        cartcoord[d] = (int)(lex / multiplier);
+        lex -= cartcoord[d] * multiplier;
+        multiplier /= latticeSize;
     }
 }
-/*init nearest neighbor sites for each element and save them in nnm and nnp*/
+/* Initialize the nearest neighbor lookup tables for a periodic lattice.
+
+    For each site on a d dimensional periodic lattice, this function determines
+    the lexicographic indices of its nearest neighbors in the + and − directions
+    along each spatial dimension.
+
+   INPUT:
+     latticeSize  – number of sites along each lattice direction (L)
+     dim          – number of spatial dimensions
+
+   OUTPUT:
+     nnp – array of lexicographic indices of forward neighbors (+ direction)
+     nnm – array of lexicographic indices of backward neighbors (− direction)
+            Both arrays have size (volume * dim), where volume = L^dim.
+
+   HOW IT WORKS:
+     Each site is represented by a unique lexicographic index "site".
+     The function converts this index to its cartesian coordinates.
+     Then, for each dimension:
+       1. It increments or decrements the coordinate (applying periodic boundary conditions),
+       2. Converts the modified coordinates back to lexicographic form,
+       3. Stores the resulting indices in the neighbor arrays.
+
+   Example:
+     In 2D (L = 4), site (x,y) = (0,3) has:
+       forward neighbors  → (1,3), (0,0)
+       backward neighbors → (3,3), (0,2)
+*/
 void init_geo(long int *restrict nnp,
            long int *restrict nnm,
-           int L,
+           int latticeSize,
            int dim)
 {
     long int volume;
-    int auxp, auxm, value;
-    long int rp, rm;
+    int coordPlus, coordMinus, coordValue;
+    long int lexFoward, lexBackward;
 
     int *cartcoord = ( int *)malloc((unsigned int)dim * sizeof(int));
-
     if (cartcoord == NULL)
     {
         fprintf(stderr, "Allocation problem at (%s, %d)\n", __FILE__, __LINE__);
         exit(EXIT_FAILURE);
     }
-
     volume = 1;
     for (int d = 0; d < dim; d++)
     {
-        volume *= L;
+        volume *= latticeSize;
     }
-  //for each element i, in lex-coord, go to cart-coordinates and maps to lex its neighb
+  //for each element i, in lexCoord, go to cartCoordinates and maps to lex its neighb
     for (long int i = 0; i < volume; i++)
     {
-        lex_to_cart(i, cartcoord, dim, L);
+        lex_to_cart(i, cartcoord, dim, latticeSize);
         for (int ii = 0; ii < dim; ii++)
         {
-            value = cartcoord[ii];
+            coordValue = cartcoord[ii];
 
-            auxp = value + 1;
-            if (auxp >= L)
+            coordPlus = coordValue + 1;
+            if (coordPlus >= latticeSize)
             {
-                auxp -= L;
+                coordPlus -= latticeSize;
             }
-            cartcoord[ii] = auxp;
-            cart_to_lex(&rp, cartcoord, dim, L);
-            nnp[dirgeo(i, ii, volume)] = rp;
+            cartcoord[ii] = coordPlus;
+            cart_to_lex(&lexFoward, cartcoord, dim, latticeSize);
+            nnp[dirgeo(i, ii, volume)] = lexFoward;
 
-            auxm = value - 1;
-            if (auxm < 0)
+            coordMinus = coordValue - 1;
+            if (coordMinus < 0)
             {
-                auxm += L;
+                coordMinus += latticeSize;
             }
-            cartcoord[ii] = auxm;
-            cart_to_lex(&rm, cartcoord, dim, L);
-            nnm[dirgeo(i, ii, volume)] = rm;
+            cartcoord[ii] = coordMinus;
+            cart_to_lex(&lexBackward, cartcoord, dim, latticeSize);
+            nnm[dirgeo(i, ii, volume)] = lexBackward;
 
-            cartcoord[ii]=value;
+            cartcoord[ii] = coordValue;
         }
     }
     free(cartcoord);
